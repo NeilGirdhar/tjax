09425b2dec637d6028a3b5c4f4c1b125677f7eb9
Neil Girdhar <mistersheik@gmail.com>
2020-08-29 04:16:18 -0400

    Fix bug with chain

--- tjax/gradient/chain.py
+++ tjax/gradient/chain.py
@@ -28,8 +28,8 @@ class ChainedGradientTransformation(GradientTransformation[List[PyTree], Weights
                parameters: Optional[Weights]) -> Tuple[Weights, List[PyTree]]:
         new_state: List[PyTree] = []
         for sub_state, transform in zip(state, self.transforms):
-            gradient, new_state = transform.update(gradient, sub_state, parameters)
-            new_state.append(new_state)
+            gradient, new_sub_state = transform.update(gradient, sub_state, parameters)
+            new_state.append(new_sub_state)
         return gradient, new_state
 
     def replace_meta_parameters(self: U,  # type: ignore
