dc31c63b45f2e3297efc5220684434ec26453464
Neil Girdhar <mistersheik@gmail.com>
2020-10-03 10:01:09 -0400

    Update the readme and simplify PyTree

--- tjax/annotations.py
+++ tjax/annotations.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Dict, Hashable, List, Sequence, Tuple, Union
+from typing import Any, Sequence, Tuple, Union
 
 from chex import Array
 
@@ -18,13 +18,4 @@ ShapeLike = Union[int, Sequence[int]]
 SliceLike = Tuple[Union[int, None, slice], ...]
 RealArray = Array
 ComplexArray = Array
-PyTree = Union[Array,
-               'PyTreeLike',
-               Tuple['PyTree', ...],
-               List['PyTree'],
-               Dict[Hashable, 'PyTree'],
-               None]
-
-
-if TYPE_CHECKING:
-    from .pytree_like import PyTreeLike
+PyTree = Any
