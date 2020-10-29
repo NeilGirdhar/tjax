c8ec06d96ad569263ec787f857d4d7f9c2aeb8b0
Neil Girdhar <mistersheik@gmail.com>
2020-10-29 08:42:30 -0400

    Add block_cotangent

--- tests/test_cotangent_tools.py
+++ tests/test_cotangent_tools.py
@@ -1,7 +1,11 @@
+from functools import partial
+
+import numpy as np
+from chex import Array
 from jax import vjp
 from numpy.testing import assert_equal
 
-from tjax import copy_cotangent, replace_cotangent
+from tjax import block_cotangent, copy_cotangent, replace_cotangent
 
 
 def test_copy_cotangent() -> None:
@@ -14,3 +18,14 @@ def test_replace_cotangent() -> None:
     p, f = vjp(replace_cotangent, 1.0, 2.0)
     assert_equal(p, 1.0)
     assert_equal(f(3.0), (2.0, 3.0))
+
+
+def test_block_cotangent() -> None:
+
+    @partial(block_cotangent, block_argnums=(1,))
+    def f(x: Array, y: Array) -> Array:
+        return x + y
+
+    p, g = vjp(f, 1.0, 2.0)
+    assert_equal(p, np.array(3.0))
+    assert_equal(g(4.0), (4.0, 0.0))
