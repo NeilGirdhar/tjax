7f5746fb6cb3779da3195afd2363ac4f0020184b
Neil Girdhar <mistersheik@gmail.com>
2021-04-09 03:00:06 -0400

    Use jax.experimental.enable_x64

--- conftest.py
+++
@@ -1,3 +0,0 @@
-from jax.config import config
-
-config.update("jax_enable_x64", True)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
+from collections.abc import Generator
+
 import numpy as np
 import pytest
+from jax.experimental import enable_x64
+
+
+@pytest.fixture(autouse=True)
+def _jax_enable64() -> Generator[None, None, None]:
+    with enable_x64():
+        yield
 
 
 @pytest.fixture
