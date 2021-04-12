0bfcf9de06c82b8d9088930106f6f4614310c802
Neil Girdhar <mistersheik@gmail.com>
2021-04-12 18:02:44 -0400

    Add BoolArray, IntegerArray; remove vjp_with_aux

--- tjax/annotations.py
+++ tjax/annotations.py
@@ -1,34 +1,27 @@
 from __future__ import annotations
 
-from typing import Any, Callable, Dict, Sequence, Tuple, TypeVar, Union
+from typing import Any, Dict, Sequence, Tuple, Union
 
 from chex import Array
-from jax import vjp
 
 __all__ = ['Shape',
            'ShapeLike',
            'SliceLike',
            'Array',
+           'BoolArray',
+           'IntegerArray',
            'RealArray',
            'ComplexArray',
            'PyTree',
-           'TapFunctionTransforms',
-           'vjp_with_aux']
+           'TapFunctionTransforms']
 
 
 Shape = Tuple[int, ...]
 ShapeLike = Union[int, Sequence[int]]
 SliceLike = Tuple[Union[int, None, slice], ...]
+BoolArray = Array
+IntegerArray = Array
 RealArray = Array
 ComplexArray = Array
 PyTree = Any
 TapFunctionTransforms = Sequence[Tuple[str, Dict[str, Any]]]
-
-
-T = TypeVar('T')
-U = TypeVar('U')
-
-
-def vjp_with_aux(fun: Callable[..., Tuple[T, U]],
-                 *primals: Any) -> Tuple[T, Callable[..., Any], U]:
-    return vjp(fun, *primals, has_aux=True)  # type: ignore
