62f5f74d4ca89784cc0273ad22f99673019f8a3a
Neil Girdhar <mistersheik@gmail.com>
2022-08-22 17:10:53 -0400

    Remove tjax.dataclass_transform

--- tjax/dataclasses.py
+++ tjax/dataclasses.py
@@ -1,5 +1,4 @@
 from ._src.dataclasses.dataclass import dataclass
-from ._src.dataclasses.dataclass_transform import dataclass_transform
 from ._src.dataclasses.helpers import as_shallow_dict, field
 
-__all__ = ['dataclass', 'dataclass_transform', 'as_shallow_dict', 'field']
+__all__ = ['dataclass', 'as_shallow_dict', 'field']
