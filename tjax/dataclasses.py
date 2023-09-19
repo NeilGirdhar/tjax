851083d7dd319df1c36c2e8a7f7ac6aafccb1a18
Neil Girdhar <mistersheik@gmail.com>
2023-09-18 20:22:16 -0400

    Expose DataclassInstance

--- tjax/dataclasses.py
+++ tjax/dataclasses.py
@@ -1,4 +1,4 @@
-from ._src.dataclasses.dataclass import TDataclassInstance, dataclass
+from ._src.dataclasses.dataclass import DataclassInstance, TDataclassInstance, dataclass
 from ._src.dataclasses.helpers import as_shallow_dict, field
 
-__all__ = ['dataclass', 'TDataclassInstance', 'as_shallow_dict', 'field']
+__all__ = ['dataclass', 'DataclassInstance', 'TDataclassInstance', 'as_shallow_dict', 'field']
