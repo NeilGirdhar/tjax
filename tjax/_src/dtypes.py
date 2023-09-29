4f71bdb1759b727c6903d7606f705694e5a35249
Neil Girdhar <mistersheik@gmail.com>
2023-09-29 05:36:05 -0400

    Work around jax.numpy._ScalarMeta

--- tjax/_src/dtypes.py
+++ tjax/_src/dtypes.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, TypedDict
+from typing import Any, TypeAlias, TypedDict
 
 import jax.numpy as jnp
 
@@ -14,21 +14,23 @@ class Tols(TypedDict):
     atol: float
 
 
-rtol_dict = {jnp.bfloat16: 1e-4,
-             jnp.float16: 1e-3, jnp.float32: 1e-4, jnp.float64: 1e-5,
-             jnp.complex64: 1e-4, jnp.complex128: 1e-5}
-atol_dict = {jnp.bfloat16: 1e-2,
-             jnp.float16: 1e-3, jnp.float32: 1e-6, jnp.float64: 1e-8,
-             jnp.complex64: 1e-6, jnp.complex128: 1e-8}
+# Work around jax.numpy._ScalarMeta
+JNumber: TypeAlias = type[jnp.number[Any]]
+rtol_dict: dict[JNumber, float] = {jnp.bfloat16.dtype.type: 1e-4, jnp.float16.dtype.type: 1e-3,
+                                   jnp.float32.dtype.type: 1e-4, jnp.float64.dtype.type: 1e-5,
+                                   jnp.complex64.dtype.type: 1e-4, jnp.complex128.dtype.type: 1e-5}
+atol_dict: dict[JNumber, float] = {jnp.bfloat16.dtype.type: 1e-2, jnp.float16.dtype.type: 1e-3,
+                                   jnp.float32.dtype.type: 1e-6, jnp.float64.dtype.type: 1e-8,
+                                   jnp.complex64.dtype.type: 1e-6, jnp.complex128.dtype.type: 1e-8}
 
 
-def default_rtol(dtype: type[jnp.number[Any]]) -> float:
+def default_rtol(dtype: JNumber) -> float:
     if not jnp.issubdtype(dtype, jnp.inexact):
         return 0.0
     return rtol_dict[dtype]
 
 
-def default_atol(dtype: type[jnp.number[Any]]) -> float:
+def default_atol(dtype: JNumber) -> float:
     if not jnp.issubdtype(dtype, jnp.inexact):
         return 1.0
     return atol_dict[dtype]
