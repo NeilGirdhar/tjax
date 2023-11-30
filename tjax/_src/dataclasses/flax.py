5f5a08955ff6293eff9fbd866e1a1fbfa12deb64
Neil Girdhar <mistersheik@gmail.com>
2023-11-30 10:34:39 -0500

    Add DataClassModule

--- tjax/_src/dataclasses/flax.py
+++ tjax/_src/dataclasses/flax.py
@@ -1,12 +1,46 @@
 from __future__ import annotations
 
-from typing import Any
+from dataclasses import KW_ONLY, InitVar, dataclass
+from typing import Any, TypeAlias
+
+from flax.experimental import nnx
+from typing_extensions import dataclass_transform, override
 
 from .helpers import field
 
-__all__ = ['module_field']
+__all__ = ['module_field', 'DataClassModule']
 
 
 def module_field(*, init: bool = False) -> Any:
     """A field that contains submodules."""
     return field(init=init, default=None, kw_only=True)  # pylint: disable=invalid-field-call
+
+
+Children: TypeAlias = tuple[nnx.State, nnx.GraphDef[Any]]
+
+
+@dataclass_transform(field_specifiers=(module_field,
+                                       nnx.field,
+                                       nnx.treenode_field,
+                                       nnx.variable_field,
+                                       nnx.param_field))
+class _DataClassModule(nnx.Module):
+    @override
+    def __init_subclass__(cls,
+                          *,
+                          init: bool = True,
+                          repr: bool = True,  # noqa: A002
+                          eq: bool = True,
+                          order: bool = False,
+                          kw_only: bool = False,
+                          **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs, experimental_pytree=True)
+        dataclass(init=init, repr=repr, eq=eq, order=order, kw_only=kw_only)(cls)
+
+
+class DataClassModule(_DataClassModule):
+    _: KW_ONLY
+    rngs: InitVar[nnx.Rngs] = nnx.field()
+
+    def __post_init__(self, rngs: nnx.Rngs) -> None:
+        pass
--- tjax/dataclasses.py
+++ tjax/dataclasses.py
@@ -1,6 +1,6 @@
 from ._src.dataclasses.dataclass import DataclassInstance, TDataclassInstance, dataclass
-from ._src.dataclasses.flax import module_field
+from ._src.dataclasses.flax import DataClassModule, module_field
 from ._src.dataclasses.helpers import as_shallow_dict, field
 
 __all__ = ['dataclass', 'DataclassInstance', 'TDataclassInstance', 'as_shallow_dict', 'field',
-           'module_field']
+           'module_field', 'DataClassModule']
