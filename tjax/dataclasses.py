168b033ecc48c252742b0cbf73b2d62556aacefe
Neil Girdhar <mistersheik@gmail.com>
2024-02-09 05:00:19 -0500

    Fix flax dependency

--- tjax/dataclasses.py
+++ tjax/dataclasses.py
@@ -1,12 +1,13 @@
 from ._src.dataclasses.dataclass import DataclassInstance, TDataclassInstance, dataclass
-from ._src.dataclasses.flax import DataClassModule, module_field
 from ._src.dataclasses.helpers import as_shallow_dict, field
 
-__all__ = [
-    'DataClassModule',
-    'DataclassInstance',
-    'TDataclassInstance',
-    'as_shallow_dict',
-    'dataclass',
-    'field',
-    'module_field']
+__all__ = ['DataclassInstance', 'TDataclassInstance', 'as_shallow_dict', 'dataclass', 'field']
+
+try:
+    import flax
+except ImportError:
+    pass
+else:
+    if flax.__version__ > '0.8':
+        from ._src.dataclasses.flax import DataClassModule, module_field
+        __all__ += ['DataClassModule', 'module_field']
