a345762c3962dcd5b40b67e292dd7e8375aeabee
Neil Girdhar <mistersheik@gmail.com>
2024-04-23 05:04:12 -0400

    Add the print_generic immediate flag

--- tjax/_src/display/print_generic.py
+++ tjax/_src/display/print_generic.py
@@ -10,6 +10,7 @@ from .internal import internal_print_generic
 
 def print_generic(*args: Any,
                   raise_on_nan: bool = True,
+                  immediate: bool = False,
                   console: Console | None = None,
                   **kwargs: Any
                   ) -> None:
@@ -17,10 +18,16 @@ def print_generic(*args: Any,
 
     Args:
         raise_on_nan: Assert if NaN is found in the output.
+        immediate: Print the value immediately even if they're tracers.  Do not wait for the
+            compiled function to be called.
         console: The console that formats the output.
         args: Positional arguments to be printed.  Only dynamic arguments are allowed.
         kwargs: Keyword arguments to be printed.  Only static keys and dynamic values are allowed.
     """
+    if immediate:
+        internal_print_generic(*args, raise_on_nan=raise_on_nan, console=console, **kwargs)
+        return
+
     leaves, tree_def = tree.flatten((args, kwargs))
 
     def callback(*tap_leaves: Any) -> None:
