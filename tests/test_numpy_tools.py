f338a30680f3159db85b5be4de2148c773b85ef7
Neil Girdhar <mistersheik@gmail.com>
2024-12-19 15:53:17 -0500

    Fix mypy error

--- tests/test_numpy_tools.py
+++ tests/test_numpy_tools.py
@@ -10,7 +10,7 @@ def test_create_diagonal_array() -> None:
     x = np.arange(prod(s), dtype=np.float64)
     y = np.reshape(x, s)
     z = create_diagonal_array(y)
-    assert z.shape == (*s, s[-1])
+    assert z.shape == (*s, s[-1])  # type: ignore[comparison-overlap]
     for index, value in np.ndenumerate(z):
         if index[-2] == index[-1]:
             y_index = index[:-1]
