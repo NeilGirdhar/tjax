476c023643b95d5d2b5a151ac924181ee3fa4e82
Neil Girdhar <mistersheik@gmail.com>
2025-02-28 12:48:15 -0500

    Make cast_to_result_type use the Array API

--- tjax/_src/dtype_tools.py
+++ tjax/_src/dtype_tools.py
@@ -2,29 +2,29 @@ from __future__ import annotations
 
 from typing import TypeVar
 
-import jax
-import jax.numpy as jnp
+from array_api_compat import array_namespace
 
+from .annotations import Array, DType, Namespace
 
-_T = TypeVar('_T', bound=tuple[jax.Array | None, ...])
+_T = TypeVar('_T', bound=tuple[Array | None, ...])
 
 
 def cast_to_result_type(arrays: _T,
-                        /,
-                        *,
-                        dtype: jax.typing.DTypeLike | None = None,
-                        ensure_inexact: bool = True
+                        *arrays_and_dtypes: type[complex | bool] | Array | DType,
+                        xp: Namespace | None = None
                         ) -> _T:
     """Casts the input arrays to a common data dtype.
 
     Args:
         arrays: Arrays to be converted (or None).
-        dtype: Overrides the inferred data type.
-        ensure_inexact: Ensures that the result type is inexact, or raises.
+        arrays_and_dtypes: Extra arrays and types to be passed to result_type.
+        xp: The optional namespace.
 
     Returns:
         The arrays cast to the inferred type.
     """
-    dtype = jnp.result_type(*arrays, dtype, *([float] if ensure_inexact else []))
-    return tuple(jnp.asarray(x, dtype)  # type: ignore # pyright: ignore
+    if xp is None:
+        xp = array_namespace(*arrays)
+    dtype = xp.result_type(*arrays, *arrays_and_dtypes)
+    return tuple(xp.asarray(x, dtype)  # type: ignore # pyright: ignore
                  if x is not None else None for x in arrays)
