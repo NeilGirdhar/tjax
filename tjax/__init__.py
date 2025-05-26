13b80fd6ee192a8bdaea150ed70af700abdad5a2
Neil Girdhar <mistersheik@gmail.com>
2025-05-25 23:02:13 -0400

    Remove k-means

--- tjax/__init__.py
+++ tjax/__init__.py
@@ -12,7 +12,6 @@ from ._src.cotangent_tools import (copy_cotangent, cotangent_combinator, print_c
                                    replace_cotangent, reverse_scale_cotangent, scale_cotangent,
                                    zero_from_primal)
 from ._src.display.display_generic import display_generic
-from ._src.timer import Timer, display_time
 from ._src.display.internal import internal_print_generic
 from ._src.display.print_generic import print_generic
 from ._src.dtype_tools import cast_to_result_type
@@ -20,7 +19,6 @@ from ._src.graph.display import graph_arrow, graph_edge_name
 from ._src.graph.register_flax import register_graph_as_nnx_node
 from ._src.graph.register_jax import register_graph_as_jax_pytree
 from ._src.graph.types_ import GraphEdgeKey, GraphNodeKey, UndirectedGraphEdgeKey
-from ._src.k_means import cluster_with_k_means
 from ._src.leaky_integral import diffused_leaky_integrate, leaky_data_weight, leaky_integrate
 from ._src.math_tools import (abs_square, divide_where, inverse_softplus, log_softplus,
                               matrix_dot_product, matrix_vector_mul, normalize, outer_product,
@@ -31,6 +29,7 @@ from ._src.rng import RngStream, create_streams, fork_streams, sample_streams
 from ._src.shims import custom_jvp, custom_jvp_method, custom_vjp, custom_vjp_method, hessian, jit
 from ._src.testing import (assert_tree_allclose, get_relative_test_string, get_test_string,
                            tree_allclose)
+from ._src.timer import Timer, display_time
 from ._src.tree_tools import dynamic_tree_all, element_count, tree_sum
 
 __all__ = [
@@ -76,7 +75,6 @@ __all__ = [
     'abstract_jit',
     'assert_tree_allclose',
     'cast_to_result_type',
-    'cluster_with_k_means',
     'copy_cotangent',
     'cotangent_combinator',
     'create_streams',
